fd222e74dad152b7279024921db263e095b8afbc
dumbus <dumbusx@gmail.com>
2025-09-21 13:56:35 +0300

    feat: add not-found-page layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { MainPage } from '../pages';
 
-const App: React.FC = () => <MainPage offersCount={322} />;
+const App: React.FC = () => (
+  <MainPage offersCount={322} />
+);
 
 export default App;
