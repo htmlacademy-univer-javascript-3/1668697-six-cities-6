b00f5bd1bc94be58baac85e28864db4908030903
dumbus <dumbusx@gmail.com>
2025-09-28 21:01:10 +0300

    feat: handle offer-review-form state change

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,5 +1,6 @@
 export * from './offer-gallery';
 export * from './offer-info';
 export * from './offer-host';
-export * from './offers-list';
+export * from './offer-review-form';
 export * from './offer-reviews-list';
+export * from './offers-list';
