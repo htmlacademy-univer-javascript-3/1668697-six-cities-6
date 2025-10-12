5e7feeb26cfb6cedb3a00cd5c36cf1ae5886c854
dumbus <dumbusx@gmail.com>
2025-10-12 18:14:01 +0300

    refactor: move header to separate widget

--- src/pages/offer-page/ui/OfferPage.tsx
+++ src/pages/offer-page/ui/OfferPage.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { AppRoute, getOffersPoints, IDetailedOffer, OfferCardType } from '../../../shared';
 import { NEAR_OFFERS_LIST_LENGTH } from '../../../shared';
 import {
+  Header,
   OfferGallery,
   OfferHost,
   OfferInfo,
@@ -44,34 +45,7 @@ export const OfferPage: React.FC<OfferPageProps> = ({ offersData }) => {
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link to={AppRoute.Main}>
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="page__main page__main--offer">
         {
