5999209ab45cf600e6a9cff321efaa5cd0ede1f1
dumbus <dumbusx@gmail.com>
2025-12-18 22:43:29 +0300

    feat: add grouping by city for favorites page

--- src/pages/favorites-page/ui/FavoritesPage.tsx
+++ src/pages/favorites-page/ui/FavoritesPage.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 
-import { OfferCardType } from '../../../shared';
-import { useAppSelector } from '../../../shared';
+import { OfferCardType, IOfferCity, ISimpleOfferInfo, AppRoute, useAppDispatch, useAppSelector } from '../../../shared';
 
-import { getFavorites, getAreFavoritesLoading, getCity } from '../../../store/slices';
+import { getFavorites, getAreFavoritesLoading, setCity } from '../../../store/slices';
 
 import { OffersList, Header, Spinner } from '../../../widgets';
 
 import { FavoritesEmptyPage } from '../../favorites-empty-page';
 
 export const FavoritesPage: React.FC = () => {
-  const city = useAppSelector(getCity);
   const favorites = useAppSelector(getFavorites);
   const areFavoritesLoading = useAppSelector(getAreFavoritesLoading);
+
+  const dispatch = useAppDispatch();
+
+  const favoritesByCity = useMemo(() => {
+    const grouped: Record<string, { city: IOfferCity; offers: ISimpleOfferInfo[] }> = {};
+
+    favorites.forEach((offer) => {
+      const cityName = offer.city.name;
+
+      if (!grouped[cityName]) {
+        grouped[cityName] = {
+          city: offer.city,
+          offers: [],
+        };
+      }
+
+      grouped[cityName].offers.push(offer);
+    });
+
+    return Object.values(grouped).sort((a, b) => a.city.name.localeCompare(b.city.name));
+  }, [favorites]);
+
+  const handleCityClick = useCallback(
+    (city: IOfferCity) => {
+      dispatch(setCity(city));
+    },
+    [dispatch]
+  );
 
   if (areFavoritesLoading) {
     return <Spinner />;
@@ -32,18 +59,23 @@ export const FavoritesPage: React.FC = () => {
             <h1 className="favorites__title">Saved listing</h1>
 
             <ul className="favorites__list">
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>{city.name}</span>
-                    </a>
+              {favoritesByCity.map(({ city, offers }) => (
+                <li key={city.name} className="favorites__locations-items">
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <Link
+                        className="locations__item-link"
+                        to={AppRoute.Main}
+                        onClick={() => handleCityClick(city)}
+                      >
+                        <span>{city.name}</span>
+                      </Link>
+                    </div>
                   </div>
-                </div>
 
-                <OffersList offers={favorites} offerCardType={OfferCardType.Favorites} />
-              </li>
-
+                  <OffersList offers={offers} offerCardType={OfferCardType.Favorites} />
+                </li>
+              ))}
             </ul>
           </section>
         </div>
