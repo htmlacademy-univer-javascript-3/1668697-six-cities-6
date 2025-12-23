0c90c83f0adcee1ae4f2adbaf9008cdd8bce4d47
dumbus <dumbusx@gmail.com>
2025-12-23 21:29:14 +0300

    feat: add tests for favorites-data selectors

--- src/store/slices/favorites-data/selectors.ts
+++ src/store/slices/favorites-data/selectors.ts
@@ -1,6 +1,6 @@
 import { NameSpace } from '../../../shared';
 import { StateType } from '../../../shared';
 
-export const getFavorites = (state: StateType) => state[NameSpace.FavoritesData].favorites;
-export const getAreFavoritesLoading = (state: StateType) => state[NameSpace.FavoritesData].areFavoritesLoading;
+export const getFavorites = (state: Pick<StateType, NameSpace.FavoritesData>) => state[NameSpace.FavoritesData].favorites;
+export const getAreFavoritesLoading = (state: Pick<StateType, NameSpace.FavoritesData>) => state[NameSpace.FavoritesData].areFavoritesLoading;
 
