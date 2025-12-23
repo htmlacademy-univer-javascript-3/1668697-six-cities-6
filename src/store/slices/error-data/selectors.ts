d925d53418d85cdfa6ac9cffb96f96df65d16540
dumbus <dumbusx@gmail.com>
2025-12-23 21:27:51 +0300

    feat: add tests for error-data selectors

--- src/store/slices/error-data/selectors.ts
+++ src/store/slices/error-data/selectors.ts
@@ -1,4 +1,4 @@
 import { NameSpace } from '../../../shared';
 import { StateType } from '../../../shared';
 
-export const getError = (state: StateType) => state[NameSpace.ErrorData].error;
+export const getError = (state: Pick<StateType, NameSpace.ErrorData>) => state[NameSpace.ErrorData].error;
