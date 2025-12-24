8031a53e1cd1c4dd9b309de3e82f332025ef3504
dumbus <dumbusx@gmail.com>
2025-12-24 22:05:01 +0300

    feat: add test for spinner (simple component)

--- src/components/widgets/spinner/ui/spinner.tsx
+++ src/components/widgets/spinner/ui/spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './spinner.css';
 
 export const Spinner: React.FC = () => (
-  <div className="wrapper">
-    <div className="spinner" />
+  <div className="wrapper" data-testid="spinner-wrapper">
+    <div className="spinner" data-testid="spinner" />
   </div>
 );
