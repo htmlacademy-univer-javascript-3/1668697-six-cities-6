c2ea7d3fd75021e253b4df910a3ce218a15f295f
dumbus <dumbusx@gmail.com>
2025-12-28 14:53:38 +0300

    feat: add tests for favorites-empty-page (complex component)

--- src/pages/favorites-empty-page/ui/favorites-empty-page.tsx
+++ src/pages/favorites-empty-page/ui/favorites-empty-page.tsx
@@ -22,7 +22,7 @@ export const FavoritesEmptyPage: React.FC = () => (
     </main>
 
     <footer className="footer">
-      <Link to={AppRoute.Main} className="footer__logo-link">
+      <Link to={AppRoute.Main} className="footer__logo-link" data-testid="footer-logo-link">
         <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
       </Link>
     </footer>
